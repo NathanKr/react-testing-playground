a02c8d4d8f8a3bbbb441d3205206705cf4d506c9
Nathan Krasney <natankrasney@gmail.com>
2020-09-30 15:48:17 +0300

    react test

--- src/test/Todos.test.js
+++ src/test/Todos.test.js
@@ -1,19 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {getQueriesForElement} from '@testing-library/dom'
+import {render} from '@testing-library/react'
 import Todos from "../components/Todos";
 
-describe("Todos test with testing-library", () => {
-  const root = document.createElement("div");
-  const {getByText} = getQueriesForElement(root);
-
-  beforeAll(() => {
-    ReactDOM.render(<Todos />, root);
-  });
+describe("Todos test with testing-library/react", () => {
+  const {getByText} = render(<Todos/>);
 
   test('Todos text exist',() =>{
-    // --- this is using testing-library/dom
+    // --- this is using testing-library/react
     expect(getByText("Add")).not.toBeNull()
   })
-
 });
