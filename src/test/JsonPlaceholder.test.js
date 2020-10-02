39f62fbeb1d2595d5b95febcb326c9053bc24f36
Nathan Krasney <natankrasney@gmail.com>
2020-10-02 12:31:19 +0300

    fix the wait bug

--- src/test/JsonPlaceholder.test.js
+++ src/test/JsonPlaceholder.test.js
@@ -1,27 +1,23 @@
 import React from "react";
-import {render , findByText } from '@testing-library/react'
+import {render , findByText ,  } from '@testing-library/react'
 import JsonPlaceholder from '../components/JsonPlaceholder'
 
 
 
 describe("JsonPlaceholder dom test", () => {
-    let getByText , getByPlaceholderText;
-    beforeEach(()=>{
-      const todos = render(<JsonPlaceholder/>);
-      getByText = todos.getByText;
-      getByPlaceholderText = todos.getByPlaceholderText;
-    })
 
     test('JsonPlaceholder text appears',()=>{
+        const {getByText} = render(<JsonPlaceholder/>);
         const elem = getByText("JsonPlaceholder")
         expect(elem).not.toBeNull();
     })
 
     // --- i have error here that i dont understand and i do not know how to fix
-    // test('first and last names appears',async () =>{
-    //     const items = await findByText("Leanne Graham")
-    //     expect(items).toHaveLength(1)
-    // })
+    test('first and last names appears',async () =>{
+        const {findByText} = render(<JsonPlaceholder/>);
+        const item =  await findByText("Leanne Graham");
+        expect(item).not.toBeNull();
+    })
 
     
 })
