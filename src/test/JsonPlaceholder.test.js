e8185e40ae9540f0f12cd3f6f04a892d24873af1
Nathan Krasney <natankrasney@gmail.com>
2020-10-02 14:05:27 +0300

    small change

--- src/test/JsonPlaceholder.test.js
+++ src/test/JsonPlaceholder.test.js
@@ -5,18 +5,17 @@ import JsonPlaceholder from '../components/JsonPlaceholder'
 
 
 describe("JsonPlaceholder dom test", () => {
-
     test('JsonPlaceholder text appears',()=>{
         const {getByText} = render(<JsonPlaceholder/>);
         const elem = getByText("JsonPlaceholder")
-        expect(elem).not.toBeNull();
+        expect(elem).toBeInTheDocument()
     })
 
     // --- i have error here that i dont understand and i do not know how to fix
     test('first and last names appears',async () =>{
         const {findByText} = render(<JsonPlaceholder/>);
         const item =  await findByText("Leanne Graham");
-        expect(item).not.toBeNull();
+        expect(item).toBeInTheDocument();
     })
 
     
